c11c4bc8971ae84699206b4fd38183036b1fbef5
Ikechukwu Miracle Nweke <nwekeiyke46@gmail.com>
2023-08-08 20:15:50 +0000

    editted 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-#prints the alphabet except 'q' and 'e'
+# prints the alphabet except 'q' and 'e'
 for alpha in range(97, 123):
     if chr(alpha) != "q" and chr(alpha) != "e":
         print("{}".format(chr(alpha)), end="")
