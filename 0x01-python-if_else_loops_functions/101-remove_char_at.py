a0e74e1788c3e83efc4f3697d45bd2a118c42ee2
Ikechukwu Miracle Nweke <nwekeiyke46@gmail.com>
2023-08-09 00:18:54 +0000

    fixed some files

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    if n < 0:
+    if n < 0 or n > len(str):
         return str
-    return ([:n] + [n + 1:])
+
+s = ""
+    for i in range(len(str)):
+        if i != n:
+            s += str[i]
+
+    return s
