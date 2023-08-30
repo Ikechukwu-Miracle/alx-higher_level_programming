f97546cb494dac318b1646f445ed9ed9a643dfa1
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2023-08-30 02:01:38 +0000

    created 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Defines a class called Square"""
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """initialzes a new instance of square
+
+        Args:
+        size(int): size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Gets and Sets the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        if not isinstance(val, int):
+            raise TypeError("size must be an integer")
+        elif val < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = val
+
+    def area(self):
+        """Returns the area of the square"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Prints the square using the '#' character.
+
+        The '#' represents the size of the square.
+        """
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+
+        if self.__size == 0:
+            print()
