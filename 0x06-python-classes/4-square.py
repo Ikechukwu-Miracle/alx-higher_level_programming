9601d7d612921b70a6698a5865c7b9ba76c29eb4
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2023-08-30 01:44:35 +0000

    created 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines a class called Square"""
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """initialzes a new instance of square
+
+        Args:
+        size(int): size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Gets and Sets the size of the square"""
+        return self__size
+
+    @size.setter
+    def size(self, val):
+        if not isinstance(val, int):
+            raise TypeError("size must be an integer")
+        elif val < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = val
+
+    def area(self):
+        """Returns the area of the square"""
+        return (self.__size * self.__size)
