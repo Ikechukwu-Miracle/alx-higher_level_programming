7c1946fb9d61798ff8334b9dc77b1fcb6e85eb22
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2023-08-30 01:32:06 +0000

    created 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a class called Square"""
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """initialzes a new instance of square
+
+        Args:
+        size(int): size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calcutes and returns the area of square from size"""
+        return (self.__size * self.__size)
