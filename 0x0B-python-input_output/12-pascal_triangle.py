e6d6f155af5d3d21e2eb6088ba391dd76958ea94
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2023-09-14 01:18:30 +0000

    created 12-pascal_trianle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a function that returns a pascal traingle
+of a given number.
+"""
+
+
+def pascal_triangle(n):
+    """Returns a pascal triangle of size n.
+
+    Arg:
+        n (int): size of the pascal triangle
+    """
+
+    if n <= 0:
+        return []
+
+    triangle = []
+    for rowNum in range(n):
+        row =[]
+        coef = 1
+
+        for k in range(rowNum + 1):
+            row.append(coef)
+            coef = coef * (rowNum - k) // (k + 1)
+
+        triangle.append(row)
+
+    return triangle
