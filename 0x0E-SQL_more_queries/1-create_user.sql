1634b60928b9185afb5c878eb1d04d9b9758869b
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2023-10-19 02:53:49 +0000

    corected 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creates MySQL server user user_0d_1 and grants all privileges
 CREATE USER IF NOT EXIST user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
