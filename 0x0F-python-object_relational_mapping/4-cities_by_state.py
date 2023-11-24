ce798cc25cc24a6a8e5050519bf655924e75b07c
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2023-11-24 21:49:39 +0000

    created 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     connection = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = connection.cursor()
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM 
                 cities INNER JOIN states ON states.id=cities.state_id""")
     results = cur.fetchall()
 
