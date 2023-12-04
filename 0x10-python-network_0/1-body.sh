5463a8106db909af75114b6f26b7af8e7c98d04a
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2023-12-04 02:08:19 +0000

    fixed my codes

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# sends a GET request to the URL, and displays the body of the response
 # shellcheck disable=SC2046
-
 if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
