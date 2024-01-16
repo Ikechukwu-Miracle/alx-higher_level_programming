488b0995a0c10c32175379083ca5245ee2b9db21
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2024-01-16 05:35:06 +0100

    created files for advance tasks

---
+++ 0x15-javascript-web_jquery/103.script.js
@@ -0,0 +1,19 @@
+// 103-script.js
+$(document).ready(function () {
+	function fetchTranslation() {
+		const languageCode = $("#language_code").val();
+		const apiUrl = `https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`;
+
+		$.get(apiUrl, function (data) {
+			$("#hello").text(data.hello);
+		});
+	}
+
+	$("#btn_translate").click(fetchTranslation);
+
+	$("#language_code").keypress(function (event) {
+		if (event.which == 13) {
+			fetchTranslation();
+		}
+	});
+});
